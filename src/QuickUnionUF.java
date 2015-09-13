3a458c9456e685685688ba9bba98bd60adaad0ab
Ryan Hamilton <ryhamz@gmail.com>
2015-09-12 23:04:05 -0500

    Added quick union class.

---
+++ src/QuickUnionUF.java
@@ -0,0 +1,33 @@
+
+public class QuickUnionUF {
+
+	private int[] id;
+	
+	//N is the number of objects
+	public quickUnionUF(int N){
+	id = new int[N];
+	//Set ID of each object to itself
+	for (int i = 0; i < N; i++)
+		id[i] = i;
+	}
+	
+	//iterates from child up to the root parent.
+	private int root(int i){
+		while (i != id[i])
+			i = id[i];
+		return i;
+	}
+	
+	
+	//checks if p and q have the same root
+	public boolean connected(int i, int q){
+		return root(p) == root(q);
+	}
+	
+	//change root of p to point to root of q
+	public void union(int p, int q){
+	int i = root(p);
+	int j = root(q);
+	id[i] = j;
+}
+}
